f7fb4a2333adc4c2fca5a9cbbfdb0f602b9ebe54
Mart <78298042+UnexplainableNo47@users.noreply.github.com>
2024-10-20 17:56:49 +0200

    20 Oct

---
+++ close.js
@@ -0,0 +1,5 @@
+const success_panel = document.getElementById('success-panel')
+
+function closePanel(success_panel){
+    success_panel.style.display = "hidden"
+}
--- validation.js
+++ validation.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('form')
 const email_input = document.getElementById('email_input')
 const error_message = document.getElementById('error-message')
+const success_panel = document.getElementById('success-panel')
+const email_text = document.getElementById('email-text')
 
 form.addEventListener('submit', (e) =>{
     //e.preventDefault() prevents submit
@@ -15,6 +17,13 @@ form.addEventListener('submit', (e) =>{
     if(errors.length > 0){
         e.preventDefault()
         error_message.innerText = errors.join(". ")
+    } else{
+        success_panel.style.display = "block"
+
+        email_text.innerText = email_input.value;
+    }
+    function closePanel(){
+        success_panel.style.display = "hidden"
     }
 })
 
@@ -25,9 +34,7 @@ function getSubscribeErrors(email){
         errors.push("Valid email required")
         email_input.classList.add('incorrect')
     }
-
     return errors;
-
 }
 
 const allInputs = [email_input]
@@ -39,3 +46,4 @@ allInputs.forEach(input =>{
         }
     })
 })
+
