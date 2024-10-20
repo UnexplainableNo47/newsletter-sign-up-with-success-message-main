c7dbac1b3cdaec0f281b2f69c5753c80cb986e11
Marty <78298042+UnexplainableNo47@users.noreply.github.com>
2024-10-20 10:28:39 +0200

    19 oct

---
+++ validation.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form')
+const email_input = document.getElementById('email_input')
+const error_message = document.getElementById('error-message')
+
+form.addEventListener('submit', (e) =>{
+    //e.preventDefault() prevents submit
+
+    let errors = []
+
+    if(email_input){
+        //find email errors
+        errors = getSubscribeErrors(email_input.value)
+    }
+
+    if(errors.length > 0){
+        e.preventDefault()
+        error_message.innerText = errors.join(". ")
+    }
+})
+
+function getSubscribeErrors(email){
+    let errors = []
+
+    if(email === '' || email == ''){
+        errors.push("Valid email required")
+        email_input.classList.add('incorrect')
+    }
+
+    return errors;
+
+}
+
+const allInputs = [email_input]
+
+allInputs.forEach(input =>{
+    input.addEventListener('input', () =>{
+        if(email_input.classList.contains('incorrect')){
+            email_input.classList.remove('incorrect')
+        }
+    })
+})
